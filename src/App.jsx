25d8e990c4da18b248c741c8488bf0bd23995244
unknown <borimskiydima2707@gmail.com>
2025-06-21 10:44:17 +0300

    upgraded site

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,49 @@
 import React from 'react'
 import './App.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    AOS.init({ duration: 800 });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="/logo.png" alt="" className="logo" />
-        <h1>FANDOM UA</h1>
-        <nav>
-          <a href="#about">О семье</a>
-          <a href="#news">Новости</a>
-          <a href="#members">Состав</a>
-          <a href="#contacts">Контакты</a>
-        </nav>
-      </header>
+       <div className="bg-bubbles">
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+         <div className="neon-line"></div>
+  <div className="neon-line neon-line2"></div>
+
+ 
+      </div>
+    <header className="App-header">
+        <div className="header-row">
+          <img src="/site-logo.webp" alt="" className="logo" />
+          <h1 className='head'>FANDOM UA</h1>
+        </div>
+  <nav>
+    <a href="#about">О семье</a>
+    <a href="#news">Новости</a>
+    <a href="#members">Состав</a>
+    <a href="#contacts">Контакты</a>
+  </nav>
+</header>
       <main>
-        <section id="about">
+        <section id="about" data-aos="fade-up">
           <h2>О семье</h2>
           <p>FANDOM UA — дружная семья игроков на 18 сервере Grand Mobile. Мы ценим активность, взаимопомощь и весёлое общение!</p>
         </section>
-        <section id="news">
+        <section id="news" data-aos="fade-up">
           <h2>Новости</h2>
           <ul>
             <li>Сайт ещё делается. Мы стараемся доделать его, как можно скорее!</li>
